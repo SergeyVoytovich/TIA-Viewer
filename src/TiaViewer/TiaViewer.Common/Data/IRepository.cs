39f5437a5412610d5c2361eb2ad6dcf68ac4f4ab
Sergey Voytovich <Sergey@DESKTOP-I8F85OV>
2021-10-07 07:51:19 -0700

    repository interface declared

---
+++ src/TiaViewer/TiaViewer.Common/Data/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TiaViewer.Common.Domain;
+
+namespace TiaViewer.Common.Data
+{
+    /// <summary>
+    /// Repository
+    /// </summary>
+    interface IRepository
+    {
+        /// <summary>
+        /// Get nodes 
+        /// </summary>
+        /// <returns>List of nodes</returns>
+        Task<IList<INode>> GetNodesAsync();
+    }
+}
