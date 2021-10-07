c79630af705feac8a0203bc71d947910f91105df
Sergey Voytovich <Sergey@DESKTOP-I8F85OV>
2021-10-07 08:00:33 -0700

    application interface provided

---
+++ src/TiaViewer/TiaViewer.Common/Application/IApplication.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TiaViewer.Common.Domain;
+
+namespace TiaViewer.Common.Application
+{
+    /// <summary>
+    /// Application
+    /// </summary>
+    /// <remarks>Provided enter point into the business logic</remarks>
+    interface IApplication
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        Task<IList<INode>> GetNodesAsync(string filePath);
+    }
+}
