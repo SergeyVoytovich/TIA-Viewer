cd6de6b23ac4006a5b418c80e5cfc3e3705b11d4
Sergey Voytovich <Sergey@DESKTOP-I8F85OV>
2021-10-07 07:45:02 -0700

    Node interface declared

---
+++ src/TiaViewer/TiaViewer.Common/Domain/INode.cs
@@ -0,0 +1,18 @@
+﻿namespace TiaViewer.Common.Domain
+{
+    /// <summary>
+    /// Node
+    /// </summary>
+    public interface INode
+    {
+        /// <summary>
+        /// Note type
+        /// </summary>
+        string Type { get; set; }
+
+        /// <summary>
+        /// Count of properties
+        /// </summary>
+        int PropertiesCount { get; set; }
+    }
+}
