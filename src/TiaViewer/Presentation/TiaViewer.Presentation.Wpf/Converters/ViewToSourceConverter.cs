cdfce3d00df9e64b07cb22383ddbdb3d527d7836
Sergey Voytovich <s.voytovich@outlook.com>
2021-10-10 15:30:14 -0700

    displaying of types rebuilded

---
+++ src/TiaViewer/Presentation/TiaViewer.Presentation.Wpf/Converters/ViewToSourceConverter.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Data;
+
+namespace TiaViewer.Presentation.Wpf.Converters
+{
+    public class ViewToSourceConverter : ConverterBase<ListCollectionView, object>
+    {
+        protected override object Convert(ListCollectionView source)
+            => source?.Groups;
+    }
+}
