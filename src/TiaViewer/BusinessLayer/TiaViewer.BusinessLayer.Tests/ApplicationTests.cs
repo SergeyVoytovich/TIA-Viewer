2d11d5d4f05fb7f2212534cc1cada6569f574bce
Sergey Voytovich <s.voytovich@outlook.com>
2021-10-10 04:42:36 -0700

    application implemented

---
+++ src/TiaViewer/BusinessLayer/TiaViewer.BusinessLayer.Tests/ApplicationTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using TiaViewer.Common.Data;
+using TiaViewer.Common.Domain;
+using Xunit;
+
+namespace TiaViewer.BusinessLayer.Tests
+{
+    public class ApplicationTests
+    {
+        [Fact]
+        public async Task GetNodesAsync_File()
+        {
+            // Prepare
+            var dataSource = new Mock<IDataSource>();
+            dataSource.Setup(d => d.GetRepository(It.IsAny<string>())).Returns(Mock.Of<IRepository>());
+            var application = new Mock<Application>(dataSource.Object) { CallBase = true };
+            application.Setup(a => a.GetNodesAsync(It.IsAny<IRepository>())).ReturnsAsync(new List<INode> { Mock.Of<INode>() });
+            const string filePath = "file.f";
+
+            // Action
+            var actual = await application.Object.GetNodesAsync(filePath);
+
+            // Asserts
+            actual.Should().NotBeNullOrEmpty();
+            application.Verify(a => a.GetNodesAsync(It.IsAny<IRepository>()));
+            dataSource.Verify(d => d.GetRepository(It.Is<string>(s => string.Equals(s, filePath, StringComparison.OrdinalIgnoreCase))));
+        }
+
+        [Fact]
+        public async Task GetNodesAsync_Repository()
+        {
+            // Prepare
+            var repository = new Mock<IRepository>();
+            repository.Setup(r => r.GetNodesAsync()).ReturnsAsync(new List<INode> { Mock.Of<INode>() });
+            var application = new Mock<Application>(Mock.Of<IDataSource>()) { CallBase = true };
+
+            // Action
+            var actual = await application.Object.GetNodesAsync(repository.Object);
+
+            // Asserts
+            actual.Should().NotBeNullOrEmpty();
+            repository.Verify(r => r.GetNodesAsync());
+        }
+    }
+}
--- src/TiaViewer/BusinessLayer/TiaViewer.BusinessLayer/Application.cs
+++ src/TiaViewer/BusinessLayer/TiaViewer.BusinessLayer/Application.cs
@@ -1,15 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TiaViewer.Common.BusinessLayer;
+using TiaViewer.Common.Data;
 using TiaViewer.Common.Domain;
 
 namespace TiaViewer.BusinessLayer
 {
     public class Application : IApplication
     {
-        public Task<IList<INode>> GetNodesAsync(string filePath)
+        #region Fields
+
+        private readonly IDataSource _dataSource;
+
+        #endregion
+
+
+        #region Constructors
+
+        public Application(IDataSource dataSource)
         {
-            throw new System.NotImplementedException();
+            _dataSource = dataSource ?? throw new ArgumentNullException(nameof(dataSource));
         }
+
+        #endregion
+
+
+        #region Methods
+
+        public Task<IList<INode>> GetNodesAsync(string filePath)
+            => GetNodesAsync(_dataSource.GetRepository(filePath));
+
+        internal virtual async Task<IList<INode>> GetNodesAsync(IRepository repository)
+            => repository is null ? new List<INode>() : await repository.GetNodesAsync();
+
+        #endregion
     }
 }
